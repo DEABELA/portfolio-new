9035a655a5049ef04c26f4d20f8014894d6c184d
DEABELA <94280319+DEABELA@users.noreply.github.com>
2022-12-13 15:23:08 +0300

    gif

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
     .add({
         targets: '#loader',
         opacity: 0,
-        duration: 1000,
+        duration: 800,
         begin: function(anim) {
             window.scrollTo(0, 0);
         }
